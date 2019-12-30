04736246b11a787b187f13e1b18fe73ab40b0e0a
DavidSanchezCabello <thorald.batallador@gmail.com>
2019-12-30 19:06:46 +0100

    Commit ejercicio 4 Refactorización #2

--- src/es/studium/TestBanco/TestBanco.java
+++ src/es/studium/TestBanco/TestBanco.java
@@ -5,41 +5,53 @@ import es.studium.Banco.Cuenta;
 
 public class TestBanco {
 
+	private static final String MONEDA = " euros.";
+	private static final String TIENE = " tiene ";
+	private static final String CUENTA_DE = "La cuenta de ";
+
 	public static void main(String[] args) {
 		/* Antonio y Beatriz se hacen cliente del banco */
 		Cliente antonio = new Cliente("123456789Z", "Antonio Alonso", "Av. Pueblo Saharaui, s/n");
 		Cliente beatriz = new Cliente("987654321A", "Beatriz Benítez", "Calle Sol, 4");
- 
+
 		/* Por defecto, todas las cuentas nuevas tienen 100€ */
-		Cuenta cuentaAntonio = new Cuenta(48151, 100, antonio);
-		Cuenta cuentaBeatriz = new Cuenta(62342, 100, beatriz);
- 
+		Cuenta AntonioAlonso = new Cuenta(48151, 100, antonio);
+		Cuenta BeatrizBenitez = new Cuenta(62342, 100, beatriz);
+
 		/* Antonio y Beatriz consultan el saldo */
-		System.out.println("La cuenta de " + cuentaAntonio.getCliente().getNombre() + " tiene "
-				+ cuentaAntonio.getSaldo() + " euros.");
-		System.out.println("La cuenta de " + cuentaBeatriz.getCliente().getNombre() + " tiene "
-				+ cuentaBeatriz.getSaldo() + " euros.");
- 
-		/* Beatriz transfiere 50€ a Antonio */
-		cuentaBeatriz.setSaldo(cuentaBeatriz.getSaldo() - 50);
-		cuentaAntonio.setSaldo(cuentaAntonio.getSaldo() + 50);
- 
-		/* Antonio y Beatriz vuelven a consultar para comprobar que todo ha ido bien */
-		System.out.println("La cuenta de " + cuentaAntonio.getCliente().getNombre() + " tiene "
-				+ cuentaAntonio.getSaldo() + " euros.");
-		System.out.println("La cuenta de " + cuentaBeatriz.getCliente().getNombre() + " tiene "
-				+ cuentaBeatriz.getSaldo() + " euros.");
- 
+		mostrar(AntonioAlonso);
+		mostrar(BeatrizBenitez);
+
+		transferir(BeatrizBenitez, AntonioAlonso);
+
+		mostrar(AntonioAlonso);
+		mostrar(BeatrizBenitez);
+
 		/* Antonio gana 100€ en una rifa y hace un ingreso en su cuenta */
-		cuentaAntonio.setSaldo(cuentaAntonio.getSaldo() + 100);
- 
+		AntonioAlonso.setSaldo(AntonioAlonso.getSaldo() + 100);
+
 		/* Beatriz tiene que pagar 30€ a hacienda y retira el dinero */
-		cuentaBeatriz.setSaldo(cuentaBeatriz.getSaldo() - 30);
- 
+		BeatrizBenitez.setSaldo(BeatrizBenitez.getSaldo() - 30);
+
 		/* Antonio transfiere 50€ a Beatriz */
+		transferir(AntonioAlonso, BeatrizBenitez);
+
+		mostrar(AntonioAlonso);
+		mostrar(BeatrizBenitez);
+
+	}
+
+	private static void mostrar(Cuenta nombreCuenta) {
+		System.out
+				.println(CUENTA_DE + nombreCuenta.getCliente().getNombre() + TIENE + nombreCuenta.getSaldo() + MONEDA);
+	}
+
+	/*
+	 * Creo un método para controlar la ejecución de las transferencias realizadas
+	 */
+	private static void transferir(Cuenta cuentaAntonio, Cuenta cuentaBeatriz) {
 		cuentaAntonio.setSaldo(cuentaAntonio.getSaldo() - 50);
 		cuentaBeatriz.setSaldo(cuentaBeatriz.getSaldo() + 50);
-
 	}
 
 }
